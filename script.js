e44a944b9242090ddb5b9a4d00b6f6627a2e78ed
sohyeonAn <ansh4393@gmail.com>
2021-10-24 23:50:41 +0900

    계획 추가 모달 3단계 추가

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 // 셀렉터
 const addPlanBtn = document.querySelector('.add-plan-button');
 const planContainer = document.querySelector('.plan-container');
-const addPlanModal = document.querySelector('.plan-add-modal');
+const addPlanModal_1 = document.querySelector('.plan-add-modal-1');
+const addPlanModal_2 = document.querySelector('.plan-add-modal-2');
+const addPlanModal_3 = document.querySelector('.plan-add-modal-3');
 const addBtn = document.querySelector('.add-button');
-const cancelBtn = document.querySelector('.cancel-button');
+const cancelBtn_1 = document.querySelector('.cancel-button-1');
+const cancelBtn_2 = document.querySelector('.cancel-button-2');
+const cancelBtn_3 = document.querySelector('.cancel-button-3');
+const nextBtn_1 = document.querySelector(".next-button-1");
+const nextBtn_2 = document.querySelector(".next-button-2");
+const prevBtn_1 = document.querySelector(".prev-button-1");
+const prevBtn_2 = document.querySelector(".prev-button-2");
 
 // 모달에서 입력받은 정보(계획명, 테이블크기)
 const planTitleInput = document.querySelector('.plan-title-input');
@@ -13,7 +21,13 @@ const tableSizeInput = document.querySelector('.table-size-input');
 document.addEventListener('DOMContentLoaded', getPlans);
 addPlanBtn.addEventListener('click', addPlanModalEvent);
 addBtn.addEventListener('click', addPlan);
-cancelBtn.addEventListener('click', closeModal);
+cancelBtn_1.addEventListener('click', closeModal);
+cancelBtn_2.addEventListener('click', closeModal);
+cancelBtn_3.addEventListener('click', closeModal);
+nextBtn_1.addEventListener('click', moveNextModal);
+nextBtn_2.addEventListener('click', moveNextModal);
+prevBtn_1.addEventListener("click", movePrevModal);
+prevBtn_2.addEventListener("click", movePrevModal);
 
 //펑션
 function getPlans() {
@@ -88,7 +102,7 @@ function getPlans() {
 }
 
 function addPlanModalEvent() {
-  addPlanModal.style.display = 'block';
+  addPlanModal_1.classList.remove(('hidden'));
 }
 
 function addPlan() {
@@ -173,7 +187,9 @@ function addPlan() {
 
 function closeModal() {
   // 모달 닫기
-  addPlanModal.style.display = 'none';
+  addPlanModal_1.classList.add('hidden');
+  addPlanModal_2.classList.add('hidden');
+  addPlanModal_3.classList.add('hidden');
 
   // 계획 추가 모달 인풋 초기화
   planTitleInput.value = '';
@@ -251,8 +267,130 @@ function planCompleteBtnEvent(e) {
 
     // 다음 셀에 계획실행완료 버튼 나타내기
     nextPlanCompleteBtn.classList.remove('hidden');
-  }else{
-    return ;
+  } else {
+    return;
   }
 
+}
+
+
+function moveNextModal(e) {
+  const clickedModalEl = e.target.parentElement;
+  let rewardsCnt = 0;
+
+  if (clickedModalEl.classList.contains('plan-add-modal-1')) {
+    // 첫번째 모달에서 다음을 클릭 했으므로 두번째 모달만 보이기
+    addPlanModal_1.classList.add('hidden');
+    addPlanModal_2.classList.remove('hidden');
+    addPlanModal_3.classList.add('hidden');
+
+    //두번째 모달 세팅하기.
+    placeOfRewardsModal(addPlanModal_2);
+  } else if (clickedModalEl.classList.contains('plan-add-modal-2')) {
+    // 두번째 모달에서 다음을 클릭 했으므로 세번째 모달만 보이기
+    addPlanModal_1.classList.add('hidden');
+    addPlanModal_2.classList.add('hidden');
+    addPlanModal_3.classList.remove('hidden');
+
+    //세번째 모달 세팅하기.
+    rewardsCnt = getRewardsCnt(addPlanModal_2);
+    inputRewardModal(addPlanModal_3, rewardsCnt);
+  }
+
+}
+
+function movePrevModal(e) {
+  const clickedModalEl = e.target.parentElement;
+
+  if (clickedModalEl.classList.contains('plan-add-modal-2')) {
+    // 두번째 모달에서 이전을 클릭 했으므로 첫번째 모달만 보이기
+    addPlanModal_1.classList.remove('hidden');
+    addPlanModal_2.classList.add('hidden');
+    addPlanModal_3.classList.add('hidden');
+
+  } else if (clickedModalEl.classList.contains('plan-add-modal-3')) {
+    // 세번째 모달에서 이전을 클릭 했으므로 두번째 모달만 보이기
+    addPlanModal_1.classList.add('hidden');
+    addPlanModal_2.classList.remove('hidden');
+    addPlanModal_3.classList.add('hidden');
+  }
+}
+
+function placeOfRewardsModal(modalEl) {
+  const stampAreaEl = modalEl.lastElementChild;
+  if (stampAreaEl.classList.contains("stamp-table-area")) {
+    stampAreaEl.remove();
+  }
+
+  const stampTableArea = document.createElement('div');
+
+  for (let i = 0; i < tableSizeInput.value; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.innerText = i + 1;
+
+    const rewardPlaceText = document.createElement('p');
+    rewardPlaceText.innerText = "";
+    rewardPlaceText.classList.add('hidden');
+    rewardPlaceText.classList.add('reward-place-text');
+    cell.appendChild(rewardPlaceText);
+    cell.addEventListener('click', placeOfRewardBtnEvent);
+
+    stampTableArea.appendChild(cell);
+  }
+
+  stampTableArea.classList.add('stamp-table-area');
+  modalEl.appendChild(stampTableArea);
+}
+
+function placeOfRewardBtnEvent(e) {
+  const clickedCell = e.target;
+  const placeText = clickedCell.children[0];
+  clickedCell.classList.toggle('clicked');
+
+  if (placeText.classList.contains('hidden')) {
+    placeText.innerText = "여기가 보상!";
+    placeText.classList.remove("hidden");
+  } else {
+    placeText.innerText = "";
+    placeText.classList.add("hidden");
+  }
+
+}
+
+function getRewardsCnt(modalEl) {
+  const stampAreaEl = modalEl.lastElementChild;
+  let rewardsCnt = 0;
+
+  for (let i = 0; i < stampAreaEl.children.length; i++) {
+    const cell = stampAreaEl.children[i];
+    if (cell.classList.contains('clicked')) {
+      rewardsCnt += 1;
+    }
+  }
+
+  return rewardsCnt;
+}
+
+function inputRewardModal(modalEl, rewardsCnt) {
+  console.log(modalEl.children);
+  for (let j = 0; j < modalEl.children.length; j++) {
+    if (modalEl.children[j].classList.contains("reward-input-area")) {
+      modalEl.children[j].remove();
+    }
+  }
+
+  for (let i = 0; i < rewardsCnt; i++) {
+    const rewardInputArea = document.createElement('div');
+    rewardInputArea.classList.add("reward-input-area");
+    const label = document.createElement('p');
+    label.innerText = `보상 ${i+1}를 입력해주세요.`;
+    const rewardInputText = document.createElement('input');
+    rewardInputText.classList.add('reward-input-text');
+    rewardInputArea.appendChild(label);
+    rewardInputArea.appendChild(rewardInputText);
+
+    modalEl.appendChild(rewardInputArea);
+
+  }
 }
